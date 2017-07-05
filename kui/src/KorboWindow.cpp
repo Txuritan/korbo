fa1938f2ebfdfaa0d3ef16fafbbb0dd363cd8d58
Txuritan <txurtian@yahoo.com>
2017-07-05 13:13:42 -0400

    Added icon/progam icon and logo.

--- kui/src/KorboWindow.cpp
+++ kui/src/KorboWindow.cpp
@@ -20,6 +20,7 @@ namespace Korbo {
             std::string i18n = settings_json["korbo"]["interface"]["language"].get<std::string>();
 
             set_title(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["title"].get<std::string>()));
+            set_icon(Gdk::Pixbuf::create_from_file("korbo.png"));
             set_size_request(800, 600);
 
             m_top_level_box.set_orientation(Gtk::ORIENTATION_VERTICAL);
@@ -29,30 +30,24 @@ namespace Korbo {
             m_menu_bar_file_item.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["file"]["name"].get<std::string>()));
             m_menu_bar_file_item.set_submenu(m_menu_bar_file_menu);
             m_menu_bar_file_menu.append(m_menu_bar_file_quit_item);
-            m_menu_bar_file_quit_item.set_label(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["file"]["quit"].get<std::string>()));
+            m_menu_bar_file_quit_item.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["file"]["quit"].get<std::string>()));
             m_menu_bar_file_quit_item.signal_activate().connect(sigc::mem_fun(*this, &on_menu_bar_file_quit_click));
 
             m_menu_bar.append(m_menu_bar_tools_item);
-            m_menu_bar_tools_item.set_label(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["tools"]["name"].get<std::string>()));
+            m_menu_bar_tools_item.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["tools"]["name"].get<std::string>()));
             m_menu_bar_tools_item.set_submenu(m_menu_bar_tools_menu);
             m_menu_bar_tools_menu.append(m_menu_bar_tools_preferences_item);
-            m_menu_bar_tools_preferences_item.set_label(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["tools"]["preferences"].get<std::string>()));
-            m_menu_bar_tools_preferences_item.signal_activate().connect(
-                    sigc::mem_fun(*this, &on_menu_bar_tools_preferences_click));
+            m_menu_bar_tools_preferences_item.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["tools"]["preferences"].get<std::string>()));
+            m_menu_bar_tools_preferences_item.signal_activate().connect(sigc::mem_fun(*this, &on_menu_bar_tools_preferences_click));
 
             m_menu_bar.append(m_menu_bar_help_item);
             m_menu_bar_help_item.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["help"]["name"].get<std::string>()));
             m_menu_bar_help_item.set_submenu(m_menu_bar_help_menu);
             m_menu_bar_help_menu.append(m_menu_bar_help_check_for_updates_item);
-            m_menu_bar_help_check_for_updates_item.set_label(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["help"]["check-for-updates"].get<std::string>()));
+            m_menu_bar_help_check_for_updates_item.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["help"]["check-for-updates"].get<std::string>()));
             m_menu_bar_help_menu.append(m_menu_bar_help_check_about_separator_item);
             m_menu_bar_help_menu.append(m_menu_bar_help_about_item);
-            m_menu_bar_help_about_item.set_label(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["help"]["about"].get<std::string>()));
+            m_menu_bar_help_about_item.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["menu"]["help"]["about"].get<std::string>()));
             m_menu_bar_help_about_item.signal_activate().connect(sigc::mem_fun(*this, &on_menu_bar_help_about_click));
 
             m_top_bottom_paned.set_orientation(Gtk::ORIENTATION_VERTICAL);
@@ -74,29 +69,21 @@ namespace Korbo {
             m_top_left_top_box.pack_start(m_profile_combo_box, TRUE, TRUE, 0);
 
             m_top_left_top_box.pack_start(m_profile_options_button, FALSE, TRUE, 0);
-            m_profile_options_button.set_label(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["buttons"]["options"].get<std::string>()));
+            m_profile_options_button.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["buttons"]["options"].get<std::string>()));
 
             m_top_left_top_box.pack_start(m_profile_backup_button, FALSE, TRUE, 0);
             m_profile_backup_button.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["buttons"]["backup"].get<std::string>()));
 
             m_top_left_top_box.pack_start(m_profile_restore_button, FALSE, TRUE, 0);
-            m_profile_restore_button.set_label(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["buttons"]["restore"].get<std::string>()));
+            m_profile_restore_button.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["buttons"]["restore"].get<std::string>()));
 
             m_top_left_box.pack_start(m_mod_list_scrolled_window, TRUE, TRUE, 0);
             m_mod_list_scrolled_window.add(m_mod_list_tree_view);
             m_mod_list_tree_view.unset_model();
             m_mods_tree_store = Gtk::TreeStore::create(m_mods_model_columns);
-            m_mod_list_tree_view.append_column(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["table"]["mods"]["enabled"].get<std::string>()),
-                    m_mods_model_columns.m_enabled_column);
-            m_mod_list_tree_view.append_column(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["table"]["mods"]["name"].get<std::string>()),
-                    m_mods_model_columns.m_name_column);
-            m_mod_list_tree_view.append_column(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["table"]["mods"]["version"].get<std::string>()),
-                    m_mods_model_columns.m_version_column);
+            m_mod_list_tree_view.append_column(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["table"]["mods"]["enabled"].get<std::string>()), m_mods_model_columns.m_enabled_column);
+            m_mod_list_tree_view.append_column(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["table"]["mods"]["name"].get<std::string>()), m_mods_model_columns.m_name_column);
+            m_mod_list_tree_view.append_column(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["table"]["mods"]["version"].get<std::string>()), m_mods_model_columns.m_version_column);
             m_mod_list_tree_view.set_model(m_mods_tree_store);
 
             m_top_left_box.pack_start(m_top_left_bottom_box, FALSE, TRUE, 0);
@@ -115,20 +102,16 @@ namespace Korbo {
             m_top_right_box.pack_start(m_notebook, TRUE, TRUE);
 
             m_notebook.append_page(m_archives_tab_box, m_archives_tab_label);
-            m_archives_tab_label.set_label(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["notebooks"]["mods"]["labels"]["archives"].get<std::string>()));
+            m_archives_tab_label.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["notebooks"]["mods"]["labels"]["archives"].get<std::string>()));
             m_archives_tab_box.pack_start(m_archives_tab_scrolled_window, TRUE, TRUE);
             m_archives_tab_scrolled_window.add(m_archives_tab_tree_view);
             m_archives_tab_tree_view.unset_model();
             m_archives_tree_store = Gtk::TreeStore::create(m_archives_model_columns);
-            m_archives_tab_tree_view.append_column(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["table"]["archives"]["name"].get<std::string>()),
-                    m_archives_model_columns.m_name_column);
+            m_archives_tab_tree_view.append_column(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["table"]["archives"]["name"].get<std::string>()), m_archives_model_columns.m_name_column);
             m_archives_tab_tree_view.set_model(m_archives_tree_store);
 
             m_notebook.append_page(m_downloads_tab_box, m_downloads_tab_label);
-            m_downloads_tab_label.set_label(
-                    Glib::ustring(i18n_json[i18n]["korbo"]["window"]["notebooks"]["mods"]["labels"]["downloads"].get<std::string>()));
+            m_downloads_tab_label.set_label(Glib::ustring(i18n_json[i18n]["korbo"]["window"]["notebooks"]["mods"]["labels"]["downloads"].get<std::string>()));
             m_downloads_tab_box.pack_start(m_downloads_tab_scrolled_window, TRUE, TRUE);
 
             m_top_bottom_paned.pack2(m_console_scrolled_window, TRUE, TRUE);
